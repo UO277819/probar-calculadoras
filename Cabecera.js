d5159fd3841145c10c968df9fcae93423aec253a
Aaron Garcia Garcia <90830415+UO277819@users.noreply.github.com>
2022-11-22 20:53:13 +0100

    Add files via upload

---
+++ Cabecera.js
@@ -0,0 +1,9 @@
+var asignatura = new Object();
+asignatura.nombre = "Software y estándares en la Web";
+asignatura.titulacion = "Grado en Ingeniería Informática del Software";
+asignatura.centro = "Escuela de Ingeniería Informática";
+asignatura.universidad = "Universidad de Oviedo";
+asignatura.curso = "2020-2021";
+var alumno = new Object();
+alumno.nombre = "Aarón García García";
+alumno.email = "uo277819@uniovi.es";
---
+++ Parrafos.js
@@ -0,0 +1,9 @@
+document.write("<p> El año actual es :");
+document.write(asignatura.curso);
+document.write("</p>");
+document.write("<p> El alumno que la cursa es :");
+document.write(alumno.nombre);
+document.write("</p>");
+document.write("<p> El email del alumno es:");
+document.write(alumno.email);
+document.write("</p>");
